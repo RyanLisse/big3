5a12d19522b40e2cdb13e40ec5313e012fb8079a
Ryan Lisse <ryan.lisse@gmail.com>
2025-11-24 22:23:13 +0100

    fix: add missing resizable component

---
+++ frontend/components/ui/resizable.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import * as ResizablePrimitive from "react-resizable-panels";
+
+const ResizablePanelGroup = React.forwardRef<
+  React.ElementRef<typeof ResizablePrimitive.PanelGroup>,
+  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelGroup>
+>(({ className, ...props }, ref) => (
+  <ResizablePrimitive.PanelGroup className={className} ref={ref} {...props} />
+));
+ResizablePanelGroup.displayName = ResizablePrimitive.PanelGroup.displayName;
+
+const ResizablePanel = React.forwardRef<
+  React.ElementRef<typeof ResizablePrimitive.Panel>,
+  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.Panel>
+>(({ className, ...props }, ref) => (
+  <ResizablePrimitive.Panel className={className} ref={ref} {...props} />
+));
+ResizablePanel.displayName = ResizablePrimitive.Panel.displayName;
+
+const ResizableHandle = React.forwardRef<
+  React.ElementRef<"div">,
+  React.ComponentPropsWithoutRef<"div"> & {
+    withHandle?: boolean;
+  }
+>(({ className, withHandle, ...props }, ref) => (
+  <div
+    className={[
+      "after:-translate-x-1/2 relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:bg-border hover:after:bg-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background",
+      withHandle &&
+        "hover:bg-accent hover:transition-colors focus-visible:bg-accent",
+      className,
+    ]
+      .filter(Boolean)
+      .join(" ")}
+    ref={ref}
+    {...props}
+  >
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+        <div className="h-0.5 w-2.5 rounded-sm bg-muted-foreground" />
+      </div>
+    )}
+  </div>
+));
+ResizableHandle.displayName = "ResizableHandle";
+
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
